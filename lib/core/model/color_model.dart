9440f0f9b981163e87b0d003ecf42ba50e8c1549
Ahmed-Soly <fmporganization22@gmail.com>
2024-12-19 23:59:41 +0200

    add color pallet list

--- lib/core/model/color_model.dart
+++ lib/core/model/color_model.dart
@@ -5,8 +5,7 @@ class ColorEntity {
 List<ColorEntity> get colorsList=>[
   ColorEntity(color: 0xff1D61E7,),
   ColorEntity(color: 0xffD48445,),
-  ColorEntity(color: 0xffEDF1F3,),
-  ColorEntity(color: 0xff1D61E7,),
-  ColorEntity(color: 0xffD48445,),
-  ColorEntity(color: 0xffEDF1F3,),
+  ColorEntity(color: 0xff537D8D,),
+  ColorEntity(color: 0xffE5D352,),
+  ColorEntity(color: 0xffAC3931,),
 ];
